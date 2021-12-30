7cadcd25ae1bdfcfe3c303d143b40dddfd40c4f3
atakan.tekoglu <atakan.tekoglu@modanisa.com>
2021-12-30 22:28:14 +0300

    [#1-issue-name][Type][User] action

---
+++ pages/about/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="about-page">
+    <h1>This is my absolutely fantastic "About Me" Page!</h1>
+    <p>Ouis aliquid non animi numquam amet voluptas dolorum sunt aspernatur commodi?</p>
+    <p>Ouis aliquid non animi numquam amet voluptas dolorum sunt aspernatur commodi?</p>
+    <p>Ouis aliquid non animi numquam amet voluptas dolorum sunt aspernatur commodi?</p>
+    <p>Ouis aliquid non animi numquam amet voluptas dolorum sunt aspernatur commodi?</p>
+  </div>
+</template>
+
+<style scoped>
+.about-page {
+  text-align: center;
+  padding: 30px;
+}
+</style>
